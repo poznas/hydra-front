d0b14145643d6f27b3ebf0c54680fc1ee80807e3
Michał Dziedzic <michal.dziedzic@schibsted.com>
2018-11-26 17:45:53 +0100

    login flow

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,22 +1,38 @@
 import { Button, Text, View } from 'react-native'
 import React, { Component } from 'react'
 import { Google } from 'expo'
+import axios from 'axios';
 
-import {IOS_CLIENT, ANDROID_CLIENT} from 'react-native-dotenv'
+import {BASE_URL, IOS_CLIENT, ANDROID_CLIENT} from 'react-native-dotenv'
 
 class LoginScreen extends Component {
 
   signInWithGoogle = async() => {
     try {
-      const result = await Google.logInAsync({
+      const googleLoginResponse = await Google.logInAsync({
         behavior: 'web',
         androidClientId: ANDROID_CLIENT,
         iosClientId: IOS_CLIENT,
         scopes: ['profile', 'email']
       });
 
-      if (result.type === 'success'){
-        console.log('succ')
+      console.log(googleLoginResponse.type);
+      console.log('\n');
+
+      if (googleLoginResponse.type === 'success'){
+        console.log(googleLoginResponse.idToken);
+        // TODO skonczyc, jak Poznas naprawi
+
+        // const url = [BASE_URL, '/auth/login'].join('');
+        // const params = {
+        //   headers: {
+        //     'X-ID-TOKEN': googleLoginResponse.idToken
+        //   }
+        // }
+        //
+        // const hydraLoginResponse = await axios.post(url, {}, params);
+        // console.log(hydraLoginResponse);
+        this.props.navigation.navigate('App');
       }
 
     } catch(e) {
