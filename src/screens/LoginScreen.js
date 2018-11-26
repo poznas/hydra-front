0d69b15bf8ed378b94ccfa6e4756063e1c834469
Michał Dziedzic <michal.dziedzic@schibsted.com>
2018-11-26 17:15:47 +0100

    setup dotenv configuration

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,39 @@
-
 import { Button, Text, View } from 'react-native'
 import React, { Component } from 'react'
+import { Google } from 'expo'
+
+import {IOS_CLIENT, ANDROID_CLIENT} from 'react-native-dotenv'
 
 class LoginScreen extends Component {
+
+  signInWithGoogle = async() => {
+    try {
+      const result = await Google.logInAsync({
+        behavior: 'web',
+        androidClientId: ANDROID_CLIENT,
+        iosClientId: IOS_CLIENT,
+        scopes: ['profile', 'email']
+      });
+
+      if (result.type === 'success'){
+        console.log('succ')
+      }
+
+    } catch(e) {
+      return {error: true};
+    }
+  }
+
   render() {
-    console.log(this.props);
+    // console.log(IOS_CLIENT);
+    // console.log(ANDROID_CLIENT);
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>LoginScreen</Text>
           <Button
               title={'Dzien dobry panie Poznanski'}
-              onPress={() => this.props.navigation.navigate('App')}
+              onPress={() => this.signInWithGoogle()}
+              // onPress={() => this.props.navigation.navigate('App')}
           />
         </View>
     );
